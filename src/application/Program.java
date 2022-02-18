9315a33e0abecc60d9d90720523376af188c5fbd
Adrian Muniz Da Silva <adrianmuniz.ceo@outlook.com>
2022-02-17 21:23:52 -0300

    Recuperando dados

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,14 +1,42 @@
 package application;
 
 import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
 
 import db.DB;
 
 public class Program {
 
 	public static void main(String[] args) {
-
-		Connection conn = DB.getConnection();
-		DB.closeConnection();
+		
+		//conexão com obanco de dados
+		Connection conn = null;
+		//consulta sql
+		Statement st = null;
+		//resultado da consulta
+		ResultSet rs= null;
+		
+		try {
+			conn = DB.getConnection();
+			
+			//instanciando
+			st = conn.createStatement();
+			
+			//comando sql e atribuindo ao resultado
+			rs = st.executeQuery("select * from department");
+			
+			//percorrendo e imprimindo
+			while(rs.next()) {
+				System.out.println(rs.getInt("id") + ", " + rs.getString("name"));
+			}
+			
+		}
+		catch (SQLException e) {
+			e.printStackTrace();
+		}
+		
+		
 	}
 }
